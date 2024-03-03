e0764cccf307382311b50de4cb2783c7fba565f3
baimiyishu13 <baimiyishu13@163.com>
2024-03-03 21:26:19 +0800

    修改告警模版

--- alertmanager-wechatrobot-webhook/transformer/transformer.go
+++ alertmanager-wechatrobot-webhook/transformer/transformer.go
@@ -18,13 +18,12 @@ func TransformToMarkdown(notification model.Notification) (markdown *model.WeCha
 	var buffer bytes.Buffer
 
 	// buffer.WriteString(fmt.Sprintf("### 当前状态:%s \n", status))
-	// buffer.WriteString(fmt.Sprintf("### 当前状态:%s \n", status))
 	// buffer.WriteString(fmt.Sprintf("#### 告警项:\n"))
 
 	for _, alert := range notification.Alerts {
 		labels := alert.Labels
 		annotations := alert.Annotations
-		buffer.WriteString(fmt.Sprintf("\n # <font color='warning'> %s\n </font>", annotations["summary"]))
+		buffer.WriteString(fmt.Sprintf("\n # 告警: <font color='warning'> %s </font>\n", annotations["summary"]))
 		buffer.WriteString(fmt.Sprintf("\n>【环境】 %s\n", labels["datacenter"]))
 		buffer.WriteString(fmt.Sprintf("\n>【级别】 %s\n", labels["severity"]))
 		buffer.WriteString(fmt.Sprintf("\n>【类型】 %s\n", labels["alertname"]))
@@ -32,9 +31,8 @@ func TransformToMarkdown(notification model.Notification) (markdown *model.WeCha
 		buffer.WriteString(fmt.Sprintf("\n>【内容】 %s\n", annotations["description"]))
 		buffer.WriteString(fmt.Sprintf("\n>【当前状态】%s \n", status))
 		buffer.WriteString(fmt.Sprintf("\n>【触发时间】 %s\n", alert.StartsAt.Format("2006-01-02 15:04:05")))
-		buffer.WriteString("\n [跳转Grafana看板](http://work.weixin.qq.com/api/doc)")
+		buffer.WriteString("\n [跳转Grafana看板](http://1.1.1.1:3000/dashboards)")
 		buffer.WriteString("\n @运维1 @运维2 ")
-
 	}
 
 	markdown = &model.WeChatMarkdown{
